11b5a1b0662b7a9f97bbffaaaf75f701cf8a4006
ABohi <askohi63907@gmail.com>
2024-04-27 09:48:13 +0600

    added

--- src/app/Hadis/HadisList/HadisMain.jsx
+++ src/app/Hadis/HadisList/HadisMain.jsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import book from "../../../../public/book (1).svg";
+import icon from "../../../../public/ওহীর সূচনা অধ্যায় - সহিহ বুখারী _ iHadis.com - Google Chrome 4_27_2024 9_15_02 AM.png";
+import { IoCopyOutline } from "react-icons/io5";
+import { CiBookmark } from "react-icons/ci"; 
+import { IoShareSocialOutline } from "react-icons/io5"; 
+import { MdOutlineReport } from "react-icons/md";
+import { MdOutlineDirections } from "react-icons/md";
 
 const HadisMain = () => {
   const [hadiths, setHadith] = useState([]);
@@ -33,7 +39,30 @@ const HadisMain = () => {
                 </div>
             }
           </div>
-          <div className="">{index}</div>
+          <div className=" bg-white p-5 my-5 rounded-[15px]">
+            <div className="flex items-center gap-2">
+              <Image className="w-[26px]" src={icon} alt="img"/>
+              <p className="text-[#2B9E76]">{hadith.id}</p>
+            </div>
+            <div className="">
+              <h1 className="my-4 text-[26px] text-end">{hadith.ar}</h1>
+              <h1 className="my-4 text-[#2B9E76]">{hadith.narrator} থেকে বর্ণিত:</h1>
+              <h1 className="my-4">{hadith.bn}</h1>
+            </div>
+            <div className="flex justify-between items-center mt-7">
+              <div className="flex items-center text-[12px] gap-3">
+              <p>হাদিসের মান :</p>
+              <button className="p-2 rounded-[8px] text-white font-bold bg-[#2B9E76]">সহিহ হাদিস</button>
+              </div>
+              <div className="flex items-center gap-6 text-[25px] text-gray-500">
+              <IoCopyOutline />
+              <CiBookmark />
+              <IoShareSocialOutline />
+              <MdOutlineReport />
+              <MdOutlineDirections />
+              </div>
+            </div>
+          </div>
         </div>
       ))}
     </div>
